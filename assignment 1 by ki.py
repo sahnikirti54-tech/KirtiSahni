c3a432ab01146dad343d2250a450e6108e4803d7
sahnikirti54-tech <sahnikirti54@gmail.com>
2026-08-19 13:59:31 +0530

    Add files via upload

---
+++ assignment 1 by ki.py
@@ -0,0 +1,97 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("hello world")
+hello world
+print('class by "M S University" is very good')
+class by "M S University" is very good
+print('class by "M S University" for \'python\' are very good.')
+class by "M S University" for 'python' are very good.
+print("MSc in Data Science\nsemester I\npython")
+MSc in Data Science
+semester I
+python
+str1 = "Hello"
+str2 ="World"
+print(str1 + " " + str2)
+Hello World
+#a
+A = 5
+print("value:", A, "I Type:", type(A), "I Adrress:", id(A))
+value: 5 I Type: <class 'int'> I Adrress: 140732857705720
+#B = 10.5
+#B = 10.5
+B = 10.5
+print("value:", B, "I Type:", type(B), "I Adrress:", id(B))
+value: 10.5 I Type: <class 'float'> I Adrress: 2565667259632
+#c
+C = "FYBCA"
+print("value:", C, "I Type:", type(C), "I Adrress:", id(C))
+value: FYBCA I Type: <class 'str'> I Adrress: 2565671992032
+a = 10
+b = 3
+print("addition (+):", a+b)
+addition (+): 13
+print("substraction (-):", a-b)
+substraction (-): 7
+print("multiplication (*):", a*b)
+multiplication (*): 30
+print("exponentiation (**):", a**b)
+exponentiation (**): 1000
+print("division (/):",  a/b)
+division (/): 3.3333333333333335
+print("floor division (//):", a//b)
+floor division (//): 3
+print("modulus (%):", a%b)
+modulus (%): 1
+
+================================== RESTART: C:/Users/StatLab/Desktop/assignment 1 DS.py ==================================
+class by "M S University" is very good
+
+================================== RESTART: C:/Users/StatLab/Desktop/assignment 1 DS.py ==================================
+class by "M S University" for 'python' are very good.
+
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+after swapping: a= 20 , b = 10
+
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+Traceback (most recent call last):
+  File "C:\Users\StatLab\Desktop\assignment 1 DS.py", line 15, in <module>
+    num1 =  float(int("enter first number:"))
+ValueError: invalid literal for int() with base 10: 'enter first number:'
+
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+Traceback (most recent call last):
+  File "C:\Users\StatLab\Desktop\assignment 1 DS.py", line 15, in <module>
+    num1 =  float(int("Enter first number: "))
+ValueError: invalid literal for int() with base 10: 'Enter first number: '
+
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+Enter first number: 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+Enter first number: 39
+Enter second number: 31
+Average: 35.0
+>>> 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+Enter first number: -50
+Enter second number: 40
+Average: -5.0
+>>> 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+enter radius of the circle: 4.9
+Traceback (most recent call last):
+  File "C:\Users\StatLab\Desktop\assignment 1 DS.py", line 25, in <module>
+    print("area of the circle: ", area)
+NameError: name 'area' is not defined
+>>> 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+enter radius of the circle: 4.9
+area of the circle:  75.42963961269095
+>>> 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+enter radius of the circle: 6
+area of the circle:  113.09733552923255
+>>> 
+================================== RESTART: C:\Users\StatLab\Desktop\assignment 1 DS.py ==================================
+enter radius of the circle: 7
+area of the circle:  153.93804002589985
