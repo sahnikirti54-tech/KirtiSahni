f006ddbd2ea002c9b34f7a5dc1d447991bc6aafe
sahnikirti54-tech <sahnikirti54@gmail.com>
2026-08-20 19:53:37 +0530

    Uploaded assignment 2 and 3

---
+++ assignment_2 by_kirti.py
@@ -0,0 +1,70 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("\n*100)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("\n"*100)
+      
+
+
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q2.py ==========================================================
+enter your name:kirti
+enter how many times to repeat(n):10
+('kirti\n\n\n\n\n\n\n\n\n\n',)
+
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q2.py ==========================================================
+enter your name:kirti
+enter how many times to repeat(n): 5
+kirti
+kirti
+kirti
+kirti
+kirti
+
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q3.py ==========================================================
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a dimond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q4.py ==========================================================
+enter month name:february
+no of days of february is : 28 or 29 ( leap year)
+
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q4.py ======
+enter month name:march
+no of days of March is :31
+>>> 
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q4.py ======
+enter month name:june
+no of days of June is :30
+>>> 
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q4.py ======
+enter month name:kirti
+invalid month name entered.
+>>> 
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5.py ==========================================================
+enter a charecter:
+=========================================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5.py ==========================================================
+enter a charecter:J
+the charecter is not a vowel (consonent).
+>>> A
+...       
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    A
+NameError: name 'A' is not defined
+>>> 
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5.py ======
+enter a charecter:A
+the charrecter is a vowel.
+>>> 
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5.py ======
+enter a charecter:E
+the charrecter is a vowel.
+>>> 
+======= RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5.py ======
+enter a charecter:D
+the charecter is not a vowel (consonent).
---
+++ assignment_3by_kirti.py
@@ -0,0 +1,217 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter charecter:e
+char, is a vowel
+
+ === RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter charecter:r
+char, is not a vowel
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter 1st number:6
+enter 2nd number:5
+6 is greater than n2
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter 1st number:7
+enter 2nd number:8
+8 is greater than n1
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter 1st number:7
+enter 2nd number:3
+enter 3rd number:4
+7 is the greatest
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter 1st number:6
+enter 2nd number:8
+enter 3rd number:5
+8 is the greatest
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter 1st number:8
+enter 2nd number:6
+enter 3rd number:4
+8 is the greatest
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter number
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter number:6
+6 is not in between 1 and 100
+101
+101
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter number:101
+101 is not in between 1 and 100
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:50
+enter mathematics marks:60
+enter biology marks:70
+enter english marks:7
+enter hindi marks:80
+Traceback (most recent call last):
+  File "C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py", line 43, in <module>
+    if percent >=90:
+NameError: name 'percent' is not defined
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:50
+enter mathematics marks:60
+enter biology marks:70
+enter english marks:80
+enter hindi marks:90
+Traceback (most recent call last):
+  File "C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py", line 44, in <module>
+    if percent >=90:
+NameError: name 'percent' is not defined. Did you mean: 'Percent'?
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:90
+enter mathematics marks:90
+enter biology marks:0
+enter english marks:90
+enter hindi marks:90
+Grade=B
+80
+80
+56
+56
+67
+67
+78
+78
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:56
+enter mathematics marks:67
+enter biology marks:78
+enter english marks:89
+enter hindi marks:15
+Traceback (most recent call last):
+  File "C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py", line 50, in <module>
+    elif persent>=60:
+NameError: name 'persent' is not defined. Did you mean: 'percent'?
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:56
+enter mathematics marks:67
+enter biology marks:78
+enter english marks:89
+enter hindi marks:15
+Grade=C
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter physics marks:0
+enter mathematics marks:9
+enter biology marks:8
+enter english marks:7
+enter hindi marks:6
+Grade=F
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+'Ram's wife is sita'
+"Ram is also known as "Maryada Purushottam" " 
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter basic salary:5000
+gross salary: 10000.0
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter basic salary:8000
+gross salary: 16000.0
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter week number6
+Saturday
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter week number8
+Rerun the program amd enter a valid number
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter week number:5
+Friday
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:8
+No. of days :31
+No. of days: 28 or 29(leap year)
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:8
+No. of days :31
+No. of days: 28 or 29(leap year)
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:8
+No. of days :31
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:5
+No. of days :31
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:2
+No. of days: 28 or 29(leap year)
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:27
+No. of days :31
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+Enter a month number:13
+Rerun the program amd enter a valid number
+
+=== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py ===
+enter amount:
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+enter amount:8000
+There are 16 notes of 500 and 0 notes of 100 with 0 rupees remaning
+6578
+6578
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+enter amount:6574
+There are 13 notes of 500 and 0 notes of 100 with 74 rupees remaning
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+enter amount:5787
+There are 11 notes of 500 and 2 notes of 100 with 87 rupees remaning
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+enter a string:hello world
+uppercase: HELLO WORLD
+lowercase: hello world
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+enter a string: HIkhyt
+uppercase:  HIKHYT
+lowercase:  hikhyt
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+Traceback (most recent call last):
+  File "C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py", line 116, in <module>
+    PRINT('STR2')
+NameError: name 'PRINT' is not defined
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+STR2
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+Traceback (most recent call last):
+  File "C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py", line 116, in <module>
+    print(STR2)
+NameError: name 'STR2' is not defined. Did you mean: 'str2'?
+>>> 
+================================== RESTART: C:/Users/Ravi Sahni/OneDrive/Desktop/ASSIGNMENT.PY/Q5 TO Q15.py =================================
+I HAVE A DOG
