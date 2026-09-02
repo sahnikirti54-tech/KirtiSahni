9533e5b80921aadc0eb6cc81167c21b282243325
sahnikirti54-tech <sahnikirti54@gmail.com>
2026-09-02 13:54:19 +0530

    Add files via upload

---
+++ assignment 1_kirti.py
@@ -0,0 +1,74 @@
+                                   #Assignment 1.
+#question 1
+'''
+print(3*"hello world ")
+'''
+#question 2a
+'''
+print('Class by "MS Univercity" is very good')
+'''
+#2b
+'''
+print(''Class by "MS University" for 'python' are very good.'')
+'''
+
+#2c
+'''
+print("MSc in data science.\n"
+      "semester 1 \n"
+      "python")
+'''
+#question 4
+'''
+print("Hello"+ " " +"world")
+'''
+#question 5
+'''
+A=5
+B=10.5
+C="FYBCA"
+print(type(A))
+print(type(B))
+print(type(C))
+'''
+#question 6
+'''
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+addition= num1 + num2;
+print(f"addition ={addition}\n");
+
+a=5
+b=4.0
+
+add = a+b
+substract = a-b
+divide = a/b
+multiply = a*b
+power = a**b
+modulo = a%b
+print (add)
+print (substract)
+print (divide)
+print (multiply)
+print (power)
+print (modulo)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
